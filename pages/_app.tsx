1853e8e1b147dcd4f67358f2d3840f4aebc3d469
Michał Hawełka <m.hawelka@gmail.com>
2022-03-28 08:07:02 +0200

    comp fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
 import "../styles/globals.css";
-import "./services/i18n";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import englishTranslations from "../locales/en/common.json";
+import polishTranslations from "../locales/pl/common.json";
+import ukrainianTranslations from "../locales/uk/common.json";
 import type { AppProps } from "next/app";
+
+const resources = {
+  en: {
+    translation: englishTranslations,
+  },
+  pl: {
+    translation: polishTranslations,
+  },
+  uk: {
+    translation: ukrainianTranslations,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  interpolation: {
+    escapeValue: false, // react already safes from xss
+  },
+});
 
 function UaFutureWebsite({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
